5d0e5bb6e6713c9c3e4f3ebbcd9453f2a3261132
Abhishek Shandilya <abhishek.sandy@gmail.com>
2016-04-19 16:06:46 +0530

    submitted after change in assignment

--- assignment#3/G_AlNi.m
+++ assignment#3/G_AlNi.m
@@ -1,9 +1,11 @@
+% total Gibbs energy
 function [outputs] = G_AlNi(T, y_Al1)
 	outputs = G_ref(T, y_Al1) ...
 			+ G_conf(T, y_Al1) ...
 			+ G_E(T, y_Al1);
 end
 
+% reference Gibbs energy
 function [outputs] = G_ref(T, y_Al1)
     y_Al2 = 0.8 - y_Al1;
     y_Ni1 = 1.0 - y_Al1;
@@ -12,6 +14,7 @@ function [outputs] = G_ref(T, y_Al1)
 			+ (y_Ni1.*y_Ni2).*G_AlNi_NiNi(T) + (y_Ni1.*y_Al2).*G_AlNi_NiAl(T);
 end
 
+% configurational Gibbs energy
 function [outputs] = G_conf(T, y_Al1)
     y_Al2 = 0.8 - y_Al1;
     y_Ni1 = 1.0 - y_Al1;
@@ -20,13 +23,18 @@ function [outputs] = G_conf(T, y_Al1)
 			+ y_Al2.*log(y_Al2) + y_Ni2.*log(y_Ni2));
 end
 
+% excess Gibbs energy
 function [outputs] = G_E(T, y_Al1)
     y_Al2 = 0.8 - y_Al1;
     y_Ni1 = 1.0 - y_Al1;
     y_Ni2 = 1.0 - y_Al2;
-	outputs = (y_Al1.*y_Ni1).*(y_Al2.*L_AlNiAl(T) + y_Ni2.*L_AlNiNi(T, y_Al1)) ...
-			+ (y_Al2.*y_Ni2).*(y_Al1.*L_AlAlNi(T) + y_Ni1.*L_NiAlNi(y_Al2));
+	outputs = (y_Al1.*y_Ni1).*(y_Al2.*L_AlNiAl(T) + y_Ni2.*L_AlNi_AlNiNi(T, y_Al1)) ...
+			+ (y_Al2.*y_Ni2).*(y_Al1.*L_AlAlNi(T) + y_Ni1.*L_AlNi_NiAlNi(y_Al2));
 end
+
+% ======================
+% Gibbs Energy values for 1st order constitutional arrays
+% ======================
 
 % G^AlNi_Al:Al
 function [outputs] = G_AlNi_AlAl(T)
@@ -49,24 +57,35 @@ function [outputs] = G_AlNi_NiAl(T)
 	outputs = G_AlNi_AlNi(T);
 end
 
-function [outputs] = L_NiAlNi(y_Al2)
+% ======================
+% L parameters = combining L0 and L1
+% ======================
+
+% L^AlNi_Ni:Al,Ni
+function [outputs] = L_AlNi_NiAlNi(y_Al2)
     y_Ni2 = 1.0 - y_Al2;
 	outputs = L0_AlNi_NiAlNi() + L1_AlNi_NiAlNi()*(y_Al2 - y_Ni2);
 end
 
-function [outputs] = L_AlNiNi(T, y_Al1)
+% L^AlNi_Al,Ni:Ni
+function [outputs] = L_AlNi_AlNiNi(T, y_Al1)
     y_Ni1 = 1.0 - y_Al1;
 	outputs = L0_AlNi_AlNiNi(T) + L1_AlNi_AlNiNi()*(y_Al1 - y_Ni1);
 end
 
-% L
+% L^AlNi_Al,Ni:Al
 function [outputs] = L_AlNiAl(T)
 	outputs = L0_AlNi_AlNiAl(T);
 end
 
+% L_Al:Al,Ni
 function [outputs] = L_AlAlNi(T)
 	outputs = L0_AlNi_AlAlNi(T);
 end
+
+% ======================
+% L0 parameters
+% ======================
 
 % L^0^AlNi_Al,Ni:Al
 function [outputs] = L0_AlNi_AlNiAl(T)
@@ -78,15 +97,19 @@ function [outputs] = L0_AlNi_AlAlNi(T)
 	outputs = L0_AlNi_AlNiAl(T);
 end
 
+% L^0^AlNi_Al,Ni:Ni
+function [outputs] = L0_AlNi_AlNiNi(T)
+	outputs = L0_AlNi_NiAlNi();
+end
+
 % L^0^AlNi_Ni:Al,Ni
 function [outputs] = L0_AlNi_NiAlNi()
 	outputs = -22050;
 end
 
-% L^0^AlNi_Al,Ni:Ni
-function [outputs] = L0_AlNi_AlNiNi(T)
-	outputs = L0_AlNi_AlAlNi(T);
-end
+% ======================
+% L1 parameters
+% ======================
 
 % L^1^AlNi_Ni:Al,Ni
 function [outputs] = L1_AlNi_NiAlNi()
@@ -97,6 +120,10 @@ end
 function [outputs] = L1_AlNi_AlNiNi()
 	outputs = L1_AlNi_NiAlNi();
 end
+
+% ======================
+% Standard state Gibbs energy values
+% ======================
 
 function [outputs] = GHSERAl(T)
 	outputs = -11278.4 + 188.684*T - 31.7482*T*log(T) - 1.231e28*T^-9;
--- assignment#3/optimize_G_AlNi.m
+++ assignment#3/optimize_G_AlNi.m
@@ -1,20 +1,30 @@
-y_Al1 = linspace(0,0.8,1e4);
+% create an array of 1e4 elements ranging from 0 to 0.8
+n = 1e4;
+y_Al1 = linspace(0,0.8,n);
+
+% Temperature (1st parameter) is given as 1200K
 G = G_AlNi(1200, y_Al1);
+% plotting Gibbs energy function to get the overall picture
 plot(y_Al1,G);
 ylabel('Gibbs energy (J/mol)');
 xlabel('Site fraction of Al in 1st sublattice');
 grid on;
 title('Gibbs energy minimization');
 
-[G_min1, ym1_Al1] = min(G);
-ym1_Al1 = 0.8*ym1_Al1/100;
+% finding the minimum value in the Gibbs energy using fminbnd
+T = 1200;
+[y_Al1_min, G_min] = fminbnd(@(y) G_AlNi(T,y),0,0.8);
+y_Al1_min
+y_Al2_min = 0.8 - y_Al1_min
+y_Ni1_min = 1 - y_Al1_min
+y_Ni2_min = 1 - y_Al2_min
+G_min
 
-G_min2 = 1e5;
-n = 1e4;
-for y_Al1 = linspace(0,0.8,n)
-    G2 = G_AlNi(1200,y_Al1);
-    if (G_min2 > G2)
-        G_min2 = G2;
-        ym2_Al1 = y_Al1;
-    end
-end
+% finding the maximum value in the Gibbs energy
+T = 1200;
+[y_Al1_max, G_max] = fminbnd(@(y) -G_AlNi(T,y),0,0.8);
+y_Al1_max
+y_Al2_max = 0.8 - y_Al1_max
+y_Ni1_max = 1 - y_Al1_max
+y_Ni2_max = 1 - y_Al2_max
+G_max
