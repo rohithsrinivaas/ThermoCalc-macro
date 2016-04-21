06dc2fa83747146bac824d0a6bece268ea0cd202
Abhishek Shandilya <abhishek.sandy@gmail.com>
2016-04-21 09:52:13 +0530

    axis labels in individual graphs

--- assignment#4/extrapolate.m
+++ assignment#4/extrapolate.m
@@ -6,33 +6,43 @@ xTi	= .3;
 % x = [.1, .3, .6-y, y];
 
 figure(1);
-plot(	xZr, Muggianu(xNb, xTi, xV, xZr), ...
-		xZr, Kohler(xNb, xTi, xV, xZr), ...
-		xZr, Colinet(xNb, xTi, xV, xZr));
+plot(	xZr*100, Muggianu(xNb, xTi, xV, xZr), ...
+		xZr*100, Kohler(xNb, xTi, xV, xZr), ...
+		xZr*100, Colinet(xNb, xTi, xV, xZr));
 legend('Muggianu', 'Kohler', 'Colinet');
 title('Extrapolated values of H mix');
 grid on;
 xlabel('at.% Zr');
-ylabel('H mix');
+ylabel('H mix (J/mol)');
 
 figure(2)
-plot(xZr, Muggianu(xNb, xTi, xV, xZr))
+plot(xZr*100, Muggianu(xNb, xTi, xV, xZr))
 title('Muggianu')
 grid on;
+xlabel('at.% Zr');
+ylabel('H mix (J/mol)');
 % H = Muggianu(xNb, xTi, xV, xZr);
 
 figure(3)
-plot(xZr, Kohler(xNb, xTi, xV, xZr))
+plot(xZr*100, Kohler(xNb, xTi, xV, xZr))
 title('Kohler')
 grid on;
+xlabel('at.% Zr');
+ylabel('H mix (J/mol)');
 % H = Kohler(xNb, xTi, xV, xZr);
 
 figure(4)
-plot(xZr, Colinet(xNb, xTi, xV, xZr));
+plot(xZr*100, Colinet(xNb, xTi, xV, xZr));
 title('Colinet')
 grid on;
+xlabel('at.% Zr');
+ylabel('H mix (J/mol)');
 % H = Colinet(xNb, xTi, xV, xZr);
 
 Muggianu_25 = Muggianu(.25,.25,.25,.25);
 Kohler_25 	= Kohler(.25,.25,.25,.25);
 Colinet_25 	= Colinet(.25,.25,.25,.25);
+
+M_min = fminbnd(@(x) Muggianu(0.1,0.3,0.6-x,x),0,0.6);
+K_min = fminbnd(@(x) Kohler(0.1,0.3,0.6-x,x),0,0.6);
+C_min = fminbnd(@(x) Colinet(0.1,0.3,0.6-x,x),0,0.6);
